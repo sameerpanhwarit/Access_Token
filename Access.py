3658d030f8fadc898438bb2f249935fba9aae8d3
S A M E E R <87935151+sameerpanhwarit@users.noreply.github.com>
2022-04-06 03:15:24 +0500

    Update Access.py

--- Access.py
+++ Access.py
@@ -2,7 +2,7 @@ import requests
 session = requests.Session()
 user=str(input("Enter Email Or Uid :"))
 pw=str(input("Enter Password: "))
-response= session.get("https://m.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+user+"&locale=en_US&password="+pw+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6").json()
+response= session.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+user+"&locale=en_US&password="+pw+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6").json()
 
 print("____________________________________")
 
